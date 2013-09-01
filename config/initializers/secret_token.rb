be64cb99db1facb3fd19fcd88b5a48bb9d573a08
Jason Wieringa <jason@jasonwieringa.com>
2013-09-01 11:13:06 -0400

    Initial commit
    'rails new railsgirls'
    
    Note: There are a few differences in my initial commit then you will
    see in yours when following the tutorial. That is because I use
    slightly different tools. For now you can ignore the differences
    because the tools accomplish the same thing. If you want to know
    more about the differences, Google for rbenv and Bundler.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '1a314f8dd7c2c39cda600cec2553bbfdc3e96186b1b8469c3a4685c962fc92a9c04e12a626d3825c9a5e0ce325ac47e7c1a4ee1e73694abf9b5d70c2489e1a3e'
